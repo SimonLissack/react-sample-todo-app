d6726620af0a1cf4907cc91f33d3c566c7b143a9
Simon Lissack <lissack.simon@gmail.com>
2020-08-12 09:00:01 +0100

    Updates

---
+++ precache-manifest.78f10564059e14c23bb1f1d4a490b95b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a7d592dc71cbaddabc9697cbbbbb5e6",
+    "url": "/react-sample-todo-app/index.html"
+  },
+  {
+    "revision": "773e7048b25540969fb0",
+    "url": "/react-sample-todo-app/static/css/main.d3faecd3.chunk.css"
+  },
+  {
+    "revision": "719b6915a0fd3986274b",
+    "url": "/react-sample-todo-app/static/js/2.d50facc3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-sample-todo-app/static/js/2.d50facc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "773e7048b25540969fb0",
+    "url": "/react-sample-todo-app/static/js/main.0804d7df.chunk.js"
+  },
+  {
+    "revision": "85e87e3e3c952fcffdf6",
+    "url": "/react-sample-todo-app/static/js/runtime-main.8a6258e9.js"
+  }
+]);
