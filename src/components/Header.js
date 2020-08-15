13e4651847b12db2bcb5a1699e5feae0249db15b
Simon Lissack <lissack.simon@gmail.com>
2020-08-15 21:14:13 +0100

    Convert Header to a functional component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,38 @@
-import React, { Component } from "react"
+import React, { useEffect, useRef } from "react"
 
-class Header extends Component {
-    componentDidUpdate(prevProps, prevState) {
+const Header = props =>{
+    const headerStyle = {
+        padding: "20px 0",
+        lineHeight: "2em",
+    };
+
+    const isInitalMount = useRef(true);
+
+    useEffect(() =>{
+        if(isInitalMount.current){
+            isInitalMount.current = false;
+            return;
+        }
+
         var r = Math.floor(Math.random() * 256);
         var g = Math.floor(Math.random() * 256);
         var b = Math.floor(Math.random() * 256);
         var bgColor = "rgb(" + r + "," + g + "," + b + ")";
 
-        if (prevProps.headerSpan !== this.props.headerSpan) {
-          document.getElementById("inH1").innerHTML = "clicked";
-          document.getElementById("inH1").style.backgroundColor = bgColor;
-        }
-    }
+        document.getElementById("inH1").innerHTML = "clicked";
+        document.getElementById("inH1").style.backgroundColor = bgColor;
+    }, [props.headerSpan]);
 
-    render() {
-        const headerStyle = {
-            padding: "20px 0",
-            lineHeight: "2em",
-        }
-        return (
-            <header style={headerStyle}>
-                <h1 style={{ fontSize: "25px", marginBottom: "15px" }}>
-                    Simple Todo App <span id="inH1"></span>
-                </h1>
-                <p style={{ fontSize: "19px" }}>
-                    Please add to-dos item(s) through the input field
-        </p>
-            </header>
-        )
-    }
+    return(
+        <header style={headerStyle}>
+            <h1 style={{ fontSize: "25px", marginBottom: "15px" }}>
+                Simple Todo App <span id="inH1"></span>
+            </h1>
+            <p style={{ fontSize: "19px" }}>
+                Please add to-dos item(s) through the input field
+            </p>
+        </header>
+    );
 }
 
 export default Header;
