7f325d64ec9f086807fb45fa7abd114dfb8ae774
Simon Lissack <lissack.simon@gmail.com>
2020-08-15 21:19:02 +0100

    Make TodoItem a functional component

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,35 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
-class TodoItem extends React.Component {
+const TodoItem = props => {
+    useEffect(() => {
+        return () => {
+            console.log("item about to be deleted");
+        }
+    }, [])
 
-    componentWillUnmount() {
-        // alert("Item about to be deleted");
-    }
+    const { completed, id, title } = props.todo;
 
-    render() {
-        const { completed, id, title } = this.props.todo;
+    const completedStyle = {
+        fontStyle: "italic",
+        color: "#d35e0f",
+        opacity: 0.4,
+        textDecoration: "line-through",
+    };
 
-        const completedStyle = {
-            fontStyle: "italic",
-            color: "#d35e0f",
-            opacity: 0.4,
-            textDecoration: "line-through",
-        };
+    return <li className="todo-item">
+        <input
+            type="checkbox"
+            checked={completed}
+            onChange={() => props.handleChangeProps(id)}
+        />
+        <span style={completed ? completedStyle : null}>
+            {title}
+        </span>
+        <button onClick={() => props.deleteTodoProps(id)}>
+            Delete
+        </button>
+    </li>
 
-        return <li className="todo-item">
-            <input
-                type="checkbox"
-                checked={completed}
-                onChange={() => this.props.handleChangeProps(id)}
-            />
-            <span style={completed ? completedStyle : null}>
-                {title}
-            </span>
-            <button onClick={() => this.props.deleteTodoProps(id)}>
-                Delete
-            </button>
-        </li>
-    }
-}
+};
 
 export default TodoItem
